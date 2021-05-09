3480784b8e9bdb6fb77bd965fc288c51f31e0604
Szabó, Máté <szabomate100@gmail.com>
2021-05-09 13:29:38 +0200

    my first commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include "graphics.hpp"
+#include "widgets.hpp"
+#include "checkbox.hpp"
+#include "applikacio.hpp"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace genv;
+
+const int cellSize = 40;
+const int cellNumber = 20;
+const int windowSize = cellSize * cellNumber;
+
+class App : public Applikacio {
+protected:
+
+public:
+
+    App() {
+        vector<vector<CheckBox>> playField;
+        for (int iy = 0; iy < cellNumber; ++iy) {
+            vector<CheckBox> playFieldRow;
+            for (int ix = 0; ix < cellNumber; ++ix) {
+                CheckBox *refCheckBox = new CheckBox(ix * cellSize, iy * cellSize, cellSize, cellSize);
+                playFieldRow.push_back(*refCheckBox);
+                bele(refCheckBox);
+            }
+            playField.push_back(playFieldRow);
+        }
+
+    }
+};
+
+int main() {
+    // parameterek
+
+    App app;
+    app.fut(windowSize, windowSize);
+    return 0;
+}
